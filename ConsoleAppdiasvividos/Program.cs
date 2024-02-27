cd4c8e9a0fe71d8c5f9ba95ae1476dc1b27978b3
Usuario <Usuario@CPD02-M13>
2024-02-27 11:52:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppdiasvividos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppdiasvividos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos anos você tem");
+             int anos = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("Qual sua idade em mes");
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual sua idade em dias");
+           int dia = int.Parse(Console.ReadLine());
+
+
+            
+            int f = (anos * 365) + (mes * 30) + dia;
+
+            Console.WriteLine("Sua idade em dias é:" + f);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
